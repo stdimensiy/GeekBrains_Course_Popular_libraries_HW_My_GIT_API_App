fcc0df744fe3adc7b125ca7f025684bc0da86e10
Dmitriy <stdimensiy@yandex.ru>
2021-08-02 22:56:26 +0300

    Практическое занятие по курсу "Популярные библиотеки: RxJava 2, Dagger 2, Moxy" Старт -  Домашнее задание к уроку №2

---
+++ app/src/main/java/ru/vdv/myapp/mygitapiapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.vdv.myapp.mygitapiapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
